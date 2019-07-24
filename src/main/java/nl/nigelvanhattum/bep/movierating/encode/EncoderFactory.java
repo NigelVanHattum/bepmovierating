4a6102d31a618a109afaceda298cc8827e13f01c
Nigel <nigelvh@hotmail.com>
2019-07-24 15:02:48 +0200

    Fixed typo

--- src/main/java/nl/nigelvanhattum/bep/movierating/encode/EncoderFactory.java
+++ src/main/java/nl/nigelvanhattum/bep/movierating/encode/EncoderFactory.java
@@ -1,12 +1,14 @@
 package nl.nigelvanhattum.bep.movierating.encode;
 
+import nl.nigelvanhattum.bep.movierating.encode.encoders.JSONEncoder;
+
 public class EncoderFactory {
 
     public static Encoder getEncoder(EncoderType type) {
 
         switch (type) {
             case JSON: return new JSONEncoder();
-            default: throw new UnsupportedOperationException("This enoder is not yet inplemented");
+            default: throw new UnsupportedOperationException("This encoder is not yet inplemented");
         }
     }
 }
