0e41144e435389ffa8c4253bd173ebf0670234bd
Nigel <nigelvh@hotmail.com>
2019-07-27 17:39:26 +0200

    Added public methods to use the model it's fields

--- src/main/java/nl/nigelvanhattum/bep/movierating/encode/model/Movie.java
+++ src/main/java/nl/nigelvanhattum/bep/movierating/encode/model/Movie.java
@@ -4,4 +4,28 @@ public class Movie {
     private String name;
     private String releaseDate;
     private String rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
 }
