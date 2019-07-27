dadccab901ad53741e6ab6fd4ded2f3ddc9c077e
Nigel <nigelvh@hotmail.com>
2019-07-27 19:09:51 +0200

    Added Unit test on model class

---
+++ src/test/java/nl/nigelvanhattum/bep/movierating/encode/model/MovieTest.java
@@ -0,0 +1,35 @@
+package nl.nigelvanhattum.bep.movierating.encode.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MovieTest {
+    Movie movie;
+
+    @Before
+    public void setMovie() {
+        movie = new Movie();
+    }
+
+    @Test
+    public void testSetName() {
+        String name = "The Godfather";
+        movie.setName(name);
+        Assert.assertEquals(name, movie.getName());
+    }
+
+    @Test
+    public void testSetRating() {
+        double rating = 9.0;
+        movie.setRating(rating);
+        Assert.assertEquals(rating, movie.getRating(), 0);
+    }
+
+    @Test
+    public void testSetReleaseDate() {
+        String releaseDate = "1994-10-14";
+        movie.setReleaseDate(releaseDate);
+        Assert.assertEquals(releaseDate, movie.getReleaseDate());
+    }
+}
