86e89f772c9927e4bf4464976af87afd1ab49476
Nigel <nigelvh@hotmail.com>
2019-07-27 22:35:54 +0200

    Added decode model

---
+++ src/main/java/nl/nigelvanhattum/bep/movierating/decode/model/Movie.java
@@ -0,0 +1,31 @@
+package nl.nigelvanhattum.bep.movierating.decode.model;
+
+public class Movie {
+    private String name;
+    private String releaseDate;
+    private double rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
