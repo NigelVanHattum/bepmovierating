fb80c0dc5bc0c8e5769bd9425ded4d1ad02d38b6
Nigel <nigelvh@hotmail.com>
2019-08-24 15:41:33 +0200

    Added more tests

--- src/test/java/nl/nigelvanhattum/bep/movierating/MainTest.java
+++ src/test/java/nl/nigelvanhattum/bep/movierating/MainTest.java
@@ -1,10 +1,17 @@
 package nl.nigelvanhattum.bep.movierating;
 
+import nl.nigelvanhattum.bep.movierating.decode.DecoderType;
+import nl.nigelvanhattum.bep.movierating.model.MovieRating;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainTest {
     Options options;
@@ -50,7 +57,48 @@ public class MainTest {
     }
 
     @Test
-    public void testEmptyInputFiles() {
-        
+    public void testDeleteFile() {
+        File file = new File("testFile.test");
+        if(!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                Assert.fail(e.getLocalizedMessage());
+            }
+        }
+        Assert.assertTrue(file.delete());
+    }
+
+    @Test
+    public void testDecodeFiles() {
+        String[] files = {"src\\test\\resources\\a.json", "src\\test\\resources\\b.json"};
+        List<MovieRating> actual = Main.decodeFiles(files, DecoderType.JSON);
+
+        ArrayList<MovieRating> expected = new ArrayList<>();
+        MovieRating movieRating1 = new MovieRating();
+        movieRating1.setName("The Shawshank Redemption");
+        movieRating1.setReleaseDate("1994-10-14");
+        movieRating1.setRating(9.2);
+        expected.add(movieRating1);
+
+        MovieRating movieRating2 = new MovieRating();
+        movieRating2.setName("The Godfather");
+        movieRating2.setReleaseDate("1972-03-24");
+        movieRating2.setRating(9.2);
+        expected.add(movieRating2);
+
+        MovieRating movieRating3 = new MovieRating();
+        movieRating3.setName("The Shawshank Redemption");
+        movieRating3.setReleaseDate("1994-10-14");
+        movieRating3.setRating(9.5);
+        expected.add(movieRating3);
+
+        MovieRating movieRating4 = new MovieRating();
+        movieRating4.setName("The Godfather");
+        movieRating4.setReleaseDate("1972-03-24");
+        movieRating4.setRating(7.5);
+        expected.add(movieRating4);
+
+        Assert.assertEquals(expected, actual);
     }
 }
