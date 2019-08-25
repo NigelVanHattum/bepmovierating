60107595274299e670d48b29846f1e4c41d9e2c2
Nigel <nigelvh@hotmail.com>
2019-08-25 20:41:12 +0200

    Added more tests on Main class

--- src/test/java/nl/nigelvanhattum/bep/movierating/MainTest.java
+++ src/test/java/nl/nigelvanhattum/bep/movierating/MainTest.java
@@ -10,8 +10,8 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainTest {
@@ -101,6 +101,60 @@ public class MainTest {
         movieRating4.setReleaseDate("1972-03-24");
         movieRating4.setRating(7.5);
         expected.add(movieRating4);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testProcessFiles() {
+        String aJson = getClass().getResource("/a.json").getFile();
+        String bJson = getClass().getResource("/b.json").getFile();
+        String cXml = getClass().getResource("/c.xml").getFile();
+        String[] jsonFiles = {aJson, bJson};
+        String[] xmlFiles = {cXml};
+
+        HashMap<String, String[]> files = new HashMap<>();
+        files.put(Main.HASHMAPKEYJSON, jsonFiles);
+        files.put(Main.HASHMAPKEYXML, xmlFiles);
+
+        List<MovieRating> actual = Main.processFiles(files);
+
+        ArrayList<MovieRating> expected = new ArrayList<>();
+        MovieRating movieRating1 = new MovieRating();
+        movieRating1.setName("The Shawshank Redemption");
+        movieRating1.setReleaseDate("1994-10-14");
+        movieRating1.setRating(9.2);
+        expected.add(movieRating1);
+
+        MovieRating movieRating2 = new MovieRating();
+        movieRating2.setName("The Godfather");
+        movieRating2.setReleaseDate("1972-03-24");
+        movieRating2.setRating(9.2);
+        expected.add(movieRating2);
+
+        MovieRating movieRating3 = new MovieRating();
+        movieRating3.setName("The Shawshank Redemption");
+        movieRating3.setReleaseDate("1994-10-14");
+        movieRating3.setRating(9.5);
+        expected.add(movieRating3);
+
+        MovieRating movieRating4 = new MovieRating();
+        movieRating4.setName("The Godfather");
+        movieRating4.setReleaseDate("1972-03-24");
+        movieRating4.setRating(7.5);
+        expected.add(movieRating4);
+
+        MovieRating movieRating5 = new MovieRating();
+        movieRating5.setName("The Shawshank Redemption");
+        movieRating5.setReleaseDate("1994-10-14");
+        movieRating5.setRating(8.0);
+        expected.add(movieRating5);
+
+        MovieRating movieRating6 = new MovieRating();
+        movieRating6.setName("The Godfather");
+        movieRating6.setReleaseDate("1972-03-24");
+        movieRating6.setRating(6.5);
+        expected.add(movieRating6);
 
         Assert.assertEquals(expected, actual);
     }
